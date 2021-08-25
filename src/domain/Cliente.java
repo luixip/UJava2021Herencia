4df877da491392f123fdc3d829f6c274ea253988
luixip <luixip@hotmail.com>
2021-08-25 12:26:28 -0500

    Clase 74. Herencia en Java (Inheritance) - parte 4

---
+++ src/domain/Cliente.java
@@ -0,0 +1,37 @@
+package domain;
+
+import java.util.Date;
+
+public class Cliente extends Persona{
+    private int idCliente;
+    private Date fechaRegistro;
+    private boolean vip;
+    private static int contadorCliente;
+
+    public Cliente(String nombre, char genero, int edad, String direccion, Date fechaRegistro, boolean vip) {
+        super(nombre, genero, edad, direccion);
+        this.idCliente = ++ Cliente.contadorCliente;
+        this.fechaRegistro = fechaRegistro;
+        this.vip = vip;
+    }
+
+    public int getIdCliente() {
+        return this.idCliente;
+    }
+
+    public Date getFechaRegistro() {
+        return this.fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public boolean isVip() {
+        return this.vip;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+}
