4148f9e55095bc6dd55bf294d07963cb531e4f6b
luixip <luixip@hotmail.com>
2021-08-25 07:56:47 -0500

    Clase 72. Herencia en Java (Inheritance) - parte 2

---
+++ src/test/TestHerencia.java
@@ -0,0 +1,10 @@
+package test;
+
+import domain.Empleado;
+
+public class TestHerencia {
+    public static void main(String[] args) {
+        Empleado empleado = new Empleado();
+        System.out.println("empleado = " + empleado);
+    }
+}
